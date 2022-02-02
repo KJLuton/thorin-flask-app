984f493cc2223cc6db41a2d3172d548cfb53cdba
Kirstyn <kirstyn.jenna@hotmail.co.uk>
2022-02-02 16:09:09 +0000

    Added contact form and POST functions.

--- run.py
+++ run.py
@@ -1,8 +1,11 @@
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
+if os.path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 
 @app.route("/")
@@ -29,8 +32,11 @@ def about_member(member_name):
     return render_template("member.html", member=member)
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        flash("Thanks {}, we have received your message!.".format(request.form.
+        get("name")))
     return render_template("contact.html", page_title="Contact")
 
 
@@ -44,3 +50,4 @@ if __name__ == "__main__":
         host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")),
         debug=True)
+
